d6c3f8aa9f6351a2bb41dd5f3959b65c1c0831dc
dzatta <daniely.zatta@escola.pr.gov.br>
2024-09-05 08:14:34 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,168 @@
+export const perguntas = [
+    {
+        enunciado: "Você desperdiça muitos alimentos em sua casa?",
+        alternativas: [
+            {
+                texto: "Sim",
+                afirmacao: [
+                    "Vamos modificar esse costume para que você não desperdice mais alimentos em sua casa.",
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "Não",
+                afirmacao: [
+                    "Você está certo disso!.",
+                ],
+                proxima: 1,
+            },
+               
+        ]
+    },
+    {
+        enunciado: "Vamos utilizar receitas de reaproveitamento! Qual você prefere?",
+        alternativas: [
+            {
+                texto: "Doce.",
+                afirmacao: [
+                    "Sugere-se que reaproveite as cascas de laranja para a produção de compotas.",
+                ],
+                proxima: 2,
+            },
+            {
+                texto: "Salgada.",
+                afirmacao: [
+                    "Sugere-se que reaproveite as cascas de ábobora e ameixa para a preparação de saladas."
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "Você gostou da ideia?",
+        alternativas: [
+            {
+                texto: "Sim.",
+                afirmacao: [
+                    "Conseguiu utilizar a IA para buscar informações úteis.",
+                    "Percebeu que a IA pode ajudar a encontrar informações úteis na internet de forma mais rápida e direcionada.",
+                    "Percebeu que a IA consegue explicar termos complicados de forma simplificada e isso ajudou muito suas pesquisas sobre assuntos complexos."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Não.",
+                afirmacao: [
+                    "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho.",
+                    "Achou que era muito mais fácil procurar por respostas utilizando alguns meios mais tradicionais mesmo que levasse mais tempo.",
+                    "Sentiu um pouco de medo de quais dados pessoais seus a IA poderia utilizar e por isso prefere fazer suas coisas com pouca intromissão da tecnologia."
+                ],
+                proxima: 3,
+            },
+        ]
+    },
+    {
+        enunciado: "Vai utilizar essa método na sua casa?",
+        alternativas: [
+            {
+                texto: "Sim.",
+                afirmacao: [
+                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
+                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
+                ],
+                proxima: 4,
+            },
+            {
+                texto: "Não.",
+                afirmacao: [
+                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
+                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Você tambem pode achar mais receitas na internet! gostou dessa ideia?",
+        alternativas: [
+            {
+                texto: "Sim.",
+                afirmacao: [
+                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
+                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Não.",
+                afirmacao: [
+                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
+                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
+                ],
+                proxima: 5,
+            },
+        ]
+    },
+    {
+        enunciado: "que tal compartilhar nossas ideias com amigos e familiares?",
+        alternativas: [
+            {
+                texto: "Acho um ótima ideia!.",
+                afirmacao: [
+                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
+                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
+                ],
+                proxima: 6,
+            },
+            {
+                texto: "Não.",
+                afirmacao: [
+                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
+                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "é muito impportante quem compartilhe, pois assim estaremos ajudando a termos um futuro melhor, concorda?",
+        alternativas: [
+            {
+                texto: "Sim!.",
+                afirmacao: [
+                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
+                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Não.",
+                afirmacao: [
+                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
+                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "Concluindo, o que achou da nossa pesquisa?",
+        alternativas: [
+            {
+                texto: "Muito boa! Fizeram um ótimo trabalho.",
+                afirmacao: [
+                    "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes.",
+                    "Ainda acha que os meios de desenho tradicionais são mais eficazes para a criatividade, por isso vem estimulando pessoas em suas redes sociais a fazer pintura em aquarela."
+                ],
+            },
+            {
+                texto: "Não gostei!.",
+                afirmacao: [
+                    "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!",
+                    "Compartilhou artes em redes sociais como forma de ensinar como se comunicar através da arte.",
+                    "Percebeu que muitas pessoas têm dificuldade em expressar suas ideias desenhando e acha que a IA é capaz de empoderar essas pessoas a tirarem ideias do papel."
+                ],
+            },
+        ]
+    }
+];
